aab73b9003a7643148a833fff3a63d3b40422da6
Andrew <andyrewly@gmail.com>
2017-05-24 18:33:00 -0700

    Removed Unused Class

--- app/src/main/java/andrewly/receiptme/model/categories/Category.java
+++
@@ -1,9 +0,0 @@
-package andrewly.receiptme.model.categories;
-
-/**
- * Created by Andrew Ly on 4/22/2017.
- */
-
-public interface Category {
-
-}
--- app/src/main/java/andrewly/receiptme/model/categories/Frozen.java
+++
@@ -1,8 +0,0 @@
-package andrewly.receiptme.model.categories;
-
-/**
- * Created by Andrew Ly on 4/28/2017.
- */
-
-public class Frozen implements Category {
-}
